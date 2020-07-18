5ef2da8b547960751d9f4799ba92c282b55d03ce
mdjibril <muhammadjibrildauda@gmail.com>
2020-07-18 17:50:40 +0100

    style header

---
+++ header.login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHOTO-ALBUM</title>
+
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimal-ui' />
+
+	<!--bootstrap-3.3.7-dist-->
+	<link rel='stylesheet' href='lib/bootstrap/css/bootstrap.min.css'>
+	<link rel='stylesheet' href='css/loggedinstyle.css'>
+
+	<script src='lib/jquery/jquery-3.2.1.min.js'></script>
+	<!--bootstrap-3.3.7-dist-->
+	<script src='lib/bootstrap/js/bootstrap.min.js'></script>
+	<style type='text/css'>
+		body{
+			font-size: 16px;
+			line-height: 1.5; 
+		}
+
+		   
+	</style>
+</head>
